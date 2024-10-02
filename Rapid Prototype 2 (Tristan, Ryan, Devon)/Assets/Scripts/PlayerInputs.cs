39cca02aa53d9c9f49216b772b6c24b184c58ce9
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-10-02 15:43:15 -0400

    Added a jump function to script

--- Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
+++ Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerInputs : MonoBehaviour
 {
     public float playerSpeed = 1.0f;  //Assigning a speed variable for the player
+    public float jumpForce = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class PlayerInputs : MonoBehaviour
         //Assiging booleans to the key inputs
         bool movingLeft = Input.GetKey(KeyCode.A); //Pressing A key will move the player left
         bool movingRight = Input.GetKey(KeyCode.D); //Pressing the D key will move the player right
+        bool hitJump = Input.GetKey(KeyCode.Space); //Pressing the Space key will force player to jump
 
         Vector3 input = new Vector3(); //This Vector3 variable is meant for recieving input from player
 
@@ -28,6 +30,10 @@ public class PlayerInputs : MonoBehaviour
         if(movingRight)
         {
             input.x += playerSpeed * Time.deltaTime;
+        }
+        if (hitJump)
+        {
+            input.y += jumpForce * Time.deltaTime;
         }
 
         transform.position += input;
