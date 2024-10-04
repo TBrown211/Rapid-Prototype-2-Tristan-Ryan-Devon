4973e970d89aee8ab041215d5c4f9df28694056d
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-10-04 12:55:27 -0400

    Organizing code into separate functions

--- Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
+++ Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
@@ -31,53 +31,29 @@ public class PlayerInputs : MonoBehaviour
     void Update()
     {
         PlayerMovement();
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            chargeTime += Time.deltaTime; //When R is held down, the charge timer increases
-           
-        }
-        if (Input.GetKeyUp(KeyCode.R) && chargeTime > 2)
-        {
-            if(facingRight)
-            {
-                //When R key is released and the charge time is over 2 seconds
-                rb2D.velocity = Vector2.right * playerSpeed; //The player is propelled to the right by velocity times player speed
-                chargeTime = 0; //Resets timer after release
-               
-            }
-            else if (!facingRight)
-            {
-                //When R key is released and the charge time is over 2 seconds
-                rb2D.velocity = Vector2.left * playerSpeed; //The player is propelled to the right by velocity times player speed
-                chargeTime = 0; //Resets timer after release
-            }
-           
-
-        }
-        if(Input.GetKeyUp(KeyCode.R) && chargeTime < 2)
-        {
-            chargeTime = 0; //This is to make sure that the timer is reset to 0
-        }
+        ChargingMechanic();        
     }
 
+    //Function for the main player controller of the game
     void PlayerMovement()
     {
         //Assiging booleans to the key inputs
         bool movingLeft = Input.GetKey(KeyCode.A); //Pressing A key will move the player left
         bool movingRight = Input.GetKey(KeyCode.D); //Pressing the D key will move the player right
-        bool hitJump = Input.GetKey(KeyCode.Space); //Pressing the Space key will force player to jump
+        bool hitJump = Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W); //Pressing the Space or W key will force player to jump
 
         Vector3 input = new Vector3(); //This Vector3 variable is meant for recieving input from player
 
         if (movingLeft)
         {
             facingRight = false;
+            knightSP.flipX = true;
             input.x += -playerSpeed * Time.deltaTime;
         }
         if (movingRight)
         {
             facingRight = true;
+            knightSP.flipX = false;
             input.x += playerSpeed * Time.deltaTime;
         }
         if (hitJump)
@@ -86,5 +62,37 @@ public class PlayerInputs : MonoBehaviour
         }
 
         transform.position += input;
+    }
+
+    //Function for the main charging mechanic
+    void ChargingMechanic()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            chargeTime += Time.deltaTime; //When R is held down, the charge timer increases
+
+        }
+        if (Input.GetKeyUp(KeyCode.R) && chargeTime > 2)
+        {
+            if (facingRight)
+            {
+                //When R key is released and the charge time is over 2 seconds
+                rb2D.velocity = Vector2.right * playerSpeed; //The player is propelled to the right by velocity times player speed
+                chargeTime = 0; //Resets timer after release
+
+            }
+            else if (!facingRight)
+            {
+                //When R key is released and the charge time is over 2 seconds
+                rb2D.velocity = Vector2.left * playerSpeed; //The player is propelled to the right by velocity times player speed
+                chargeTime = 0; //Resets timer after release
+            }
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.R) && chargeTime < 2)
+        {
+            chargeTime = 0; //This is to make sure that the timer is reset to 0
+        }
     }
 }
