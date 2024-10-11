2ba24f9120c49cd6e547ba2a1ac002033656fedf
COdinRyT <150468038+COdinRyT@users.noreply.github.com>
2024-10-11 17:33:29 -0400

    Delete Uneccessary Code

--- Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
+++ Rapid Prototype 2 (Tristan, Ryan, Devon)/Assets/Scripts/PlayerInputs.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
 using UnityEngine;
 
 public class PlayerInputs : MonoBehaviour
